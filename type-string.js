3854f969d2aaa33ef7757c9af7157efd5d3a143f
Ayron Santos <ayronssantos@gmail.com>
2022-02-17 10:06:00 -0300

    exemplos da aula 01

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = 'Olá, Mundo!';
+const texto2 = "Olá, mundo!";
+const senha = "senhasupersegura456";
+const stringDeNumeros = "34567"
+
+const citacao = 'Meu nome é: ';
+const meuNome = "Leonardo"
+
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
+
