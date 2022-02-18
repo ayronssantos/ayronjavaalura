4d2f2cd327cefe13c105bdc4e700536c321853bb
Ayron Santos <ayronssantos@gmail.com>
2022-02-18 11:26:18 -0300

    adicionado conversoes trhutyefalsy e varlet

---
+++ conversões.js
@@ -0,0 +1,17 @@
+// tipo de dado
+//booleanos
+
+//conversão implicita
+
+
+const numero = 456;
+const numerString = "456";
+
+//Number()
+//String()
+
+console.log(numero + Number(numerString))
+
+
+//conversão explicita
+
---
+++ var-let-const.js
@@ -0,0 +1,35 @@
+// var
+
+// var altura = 5;
+// var comprimento = 7;
+
+// area = altura * comprimento;
+// console.log(area)
+// var area;
+
+//let
+
+// let forma = retangulo;
+// let altura = 5;
+// let comprimento = 7;
+
+// if(forma === ' retangulo'){
+// area = altura * comprimento
+// }else{
+//     area = (altura * comrimento) / 2
+// }
+// console.log(area)
+
+//const
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+
+const area = altura * comprimento;
+console.log(area);
+
+if (forma === 'quadrado'){
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2
+}
